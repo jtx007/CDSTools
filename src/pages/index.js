f5b307506e8123ed8f7d54aff8a8a611a9f99548
jtx007 <jamesjacobthomas7@gmail.com>
2020-04-11 23:24:27 -0400

    refactor(changed file structure of directories): fixed weird multi directory bug with file tree

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Layout from './components/Layout'
+import Layout from '../components/Layout'
 
 export default () => (
-<div><Layout></Layout></div>
+<div><Layout></Layout>Hey Owais!</div>
 )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,3 +1,3 @@
 import React from 'react';
-import Layout from './components/Layout'
+import Layout from '../components/Layout'
 export default () => <div><Layout></Layout>Welcome to page 2!</div>
