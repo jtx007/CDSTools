d41531dd5d3e45a45204191c970926097180b2a1
jtx007 <jamesjacobthomas7@gmail.com>
2020-04-13 03:54:26 -0400

    changed font size of lats updated header

--- src/templates/mdtemplate.js
+++ src/templates/mdtemplate.js
@@ -12,7 +12,7 @@ export default function Template({
         <div className="blog-post-container">
           <div className="blog-post">
             <h1>{frontmatter.title}</h1>
-            <h2>Last Updated: {frontmatter.date}</h2>
+            <h4>Last Updated: {frontmatter.date}</h4>
             <div
               className="blog-post-content"
               dangerouslySetInnerHTML={{ __html: html }}
